479d18c46ce33a2b599d86788f5aeaaece9896c1
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-12-18 19:42:44 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DEPLOY REACT APP IN DROPLET 
+          GITHUB ACTIONS EXAMPLE CONTINUOUS DEPLOY
         </p>
         <a
           className="App-link"
